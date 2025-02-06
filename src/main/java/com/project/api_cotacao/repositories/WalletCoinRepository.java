0e91c0f7326492d9ace74a727321c089eca4e8f1
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-02-06 10:05:23 -0300

    feat: Updating WalletCoinRepository

--- src/main/java/com/project/api_cotacao/repositories/WalletCoinRepository.java
+++ src/main/java/com/project/api_cotacao/repositories/WalletCoinRepository.java
@@ -1,7 +1,10 @@
 package com.project.api_cotacao.repositories;
 
+import com.project.api_cotacao.entities.coin.CoinEntity;
 import com.project.api_cotacao.entities.wallet.WalletCoinEntity;
+import com.project.api_cotacao.entities.wallet.WalletEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WalletCoinRepository extends JpaRepository<WalletCoinEntity,Long> {
+    WalletCoinEntity findByWalletAndCoin(WalletEntity wallet, CoinEntity coin);
 }
