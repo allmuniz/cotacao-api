625051f66cb3187e86745a7479a59baa8970e15a
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-02-06 10:07:56 -0300

    feat: Updating user and wallet service

--- src/main/java/com/project/api_cotacao/services/UserService.java
+++ src/main/java/com/project/api_cotacao/services/UserService.java
@@ -1,9 +1,9 @@
 package com.project.api_cotacao.services;
 
+import com.project.api_cotacao.entities.coin.CoinEntity;
 import com.project.api_cotacao.entities.user.UserEntity;
 import com.project.api_cotacao.entities.user.dtos.UserRequestDto;
 import com.project.api_cotacao.entities.user.dtos.UserResponseAllDto;
-import com.project.api_cotacao.entities.user.dtos.UserResponseBalanceDto;
 import com.project.api_cotacao.entities.user.exceptions.UserFoundException;
 import com.project.api_cotacao.repositories.UserRepository;
 import org.springframework.http.HttpStatus;
@@ -14,26 +14,41 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final CoinService coinService;
+    private final WalletCoinService walletCoinService;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, CoinService coinService, WalletCoinService walletCoinService) {
         this.userRepository = userRepository;
+        this.coinService = coinService;
+        this.walletCoinService = walletCoinService;
     }
 
     public ResponseEntity<UserResponseAllDto> createUser(UserRequestDto request) {
 
-        UserEntity user = userRepository.findByEmail(request.email());
+        UserEntity user = getUserByEmail(request.email());
         if (user != null) throw new UserFoundException("User already exists");
 
-        UserEntity newUser = new UserEntity(request);
+        CoinEntity coin = this.coinService.getCoinById(1L);
+
+        UserEntity newUser = new UserEntity(request, coin);
         userRepository.save(newUser);
+
+        this.walletCoinService.createWalletCoin(newUser.getWallet(), coin);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(UserResponseAllDto.fromEntity(newUser));
     }
 
     /*public ResponseEntity<UserResponseBalanceDto> updatePrincipalBalance(Long id) {
-        UserEntity user = getUser(id);
+        UserEntity user = getUserById(id);
+
+
     }*/
 
-    public UserEntity getUser(Long id) {
+    public UserEntity getUserById(Long id) {
         return userRepository.findById(id).orElseThrow(() -> new UserFoundException("User not found"));
+    }
+
+    public UserEntity getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 }
--- src/main/java/com/project/api_cotacao/services/WalletService.java
+++ src/main/java/com/project/api_cotacao/services/WalletService.java
@@ -1,15 +1,36 @@
 package com.project.api_cotacao.services;
 
+import com.project.api_cotacao.entities.wallet.WalletCoinEntity;
+import com.project.api_cotacao.entities.wallet.WalletEntity;
+import com.project.api_cotacao.entities.wallet.dtos.WalletCoinRequestDto;
+import com.project.api_cotacao.entities.wallet.dtos.WalletCoinResponseDto;
+import com.project.api_cotacao.entities.wallet.enums.TransactionType;
+import com.project.api_cotacao.entities.wallet.exceptions.WalletNotFoundException;
 import com.project.api_cotacao.repositories.WalletRepository;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WalletService {
 
     private final WalletRepository walletRepository;
+    private final WalletCoinService walletCoinService;
 
-    public WalletService(WalletRepository walletRepository) {
+    public WalletService(WalletRepository walletRepository, @Lazy WalletCoinService walletCoinService) {
         this.walletRepository = walletRepository;
+        this.walletCoinService = walletCoinService;
     }
 
+    public ResponseEntity<WalletCoinResponseDto> handleTransaction(Long walletId, WalletCoinRequestDto dto, TransactionType type) {
+        WalletEntity wallet = getWalletById(walletId);
+        WalletCoinEntity coin = walletCoinService.getWalletCoinByWalletAndCoin(wallet, wallet.getPrincipalCode());
+
+        boolean isDeposit = type == TransactionType.DEPOSIT;
+        return walletCoinService.updateWalletCoin(coin, dto.balance(), isDeposit);
+    }
+
+    public WalletEntity getWalletById(Long id) {
+    return walletRepository.findById(id).orElseThrow(() -> new WalletNotFoundException("Wallet not found"));
+    }
 }
