2d66bd2d08f5e24023dd7159bb3dd5b27f1fec42
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-02-06 10:06:44 -0300

    feat: Creating WalletCoin dtos

---
+++ src/main/java/com/project/api_cotacao/entities/wallet/dtos/WalletCoinRequestDto.java
@@ -0,0 +1,4 @@
+package com.project.api_cotacao.entities.wallet.dtos;
+
+public record WalletCoinRequestDto(Double balance) {
+}
---
+++ src/main/java/com/project/api_cotacao/entities/wallet/dtos/WalletCoinResponseDto.java
@@ -0,0 +1,4 @@
+package com.project.api_cotacao.entities.wallet.dtos;
+
+public record WalletCoinResponseDto (String code, Double balance){
+}
