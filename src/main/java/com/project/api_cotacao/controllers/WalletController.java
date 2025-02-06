c60d8b1f16b467f245ae7c29ea0d806cc73ea007
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-02-06 10:08:26 -0300

    feat: Creating wallet controller

---
+++ src/main/java/com/project/api_cotacao/controllers/WalletController.java
@@ -0,0 +1,28 @@
+package com.project.api_cotacao.controllers;
+
+import com.project.api_cotacao.entities.wallet.dtos.WalletCoinRequestDto;
+import com.project.api_cotacao.entities.wallet.dtos.WalletCoinResponseDto;
+import com.project.api_cotacao.entities.wallet.enums.TransactionType;
+import com.project.api_cotacao.services.WalletService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/wallet")
+public class WalletController {
+
+    private final WalletService walletService;
+
+    public WalletController(WalletService walletService) {
+        this.walletService = walletService;
+    }
+
+    @PutMapping("/{type}/{walletId}")
+    public ResponseEntity<WalletCoinResponseDto> handleTransaction(
+            @PathVariable Long walletId,
+            @PathVariable TransactionType type,
+            @RequestBody WalletCoinRequestDto dto) {
+
+        return walletService.handleTransaction(walletId, dto, type);
+    }
+}
