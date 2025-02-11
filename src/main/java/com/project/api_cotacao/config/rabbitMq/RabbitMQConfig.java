0313376e5b467d9de9b04f4f36fdd252fe37c400
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-02-11 09:43:10 -0300

    feat: Creating RabbitMq configuration

---
+++ src/main/java/com/project/api_cotacao/config/rabbitMq/RabbitMQConfig.java
@@ -0,0 +1,26 @@
+package com.project.api_cotacao.config.rabbitMq;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "emailQueue";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(org.springframework.amqp.rabbit.connection.ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+}
+
