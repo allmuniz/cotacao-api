6ab20f1f81468f77091f356b4d779b5d415f421b
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-02-11 10:16:14 -0300

    feat: Updating the user's CRUD in the controller

--- src/main/java/com/project/api_cotacao/controllers/UserController.java
+++ src/main/java/com/project/api_cotacao/controllers/UserController.java
@@ -2,11 +2,13 @@ package com.project.api_cotacao.controllers;
 
 import com.project.api_cotacao.config.security.dtos.AuthRequestDto;
 import com.project.api_cotacao.config.security.dtos.AuthResponseDto;
+import com.project.api_cotacao.entities.user.UserEntity;
 import com.project.api_cotacao.entities.user.dtos.UserRequestDto;
 import com.project.api_cotacao.entities.user.dtos.UserResponseAllDto;
 import com.project.api_cotacao.services.UserService;
 import com.project.api_cotacao.services.security.AuthService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,13 +23,32 @@ public class UserController {
         this.authService = authService;
     }
 
+    @PostMapping("/auth")
+    public ResponseEntity<AuthResponseDto> login(@RequestBody AuthRequestDto request) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+
     @PostMapping("/create")
     public ResponseEntity<UserResponseAllDto> createUser(@RequestBody UserRequestDto request) {
         return userService.createUser(request);
     }
 
-    @PostMapping("/auth")
-    public ResponseEntity<AuthResponseDto> login(@RequestBody AuthRequestDto request) {
-        return ResponseEntity.ok(authService.authenticate(request));
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponseAllDto> findUser(@PathVariable Long userId) {
+        return userService.findUser(userId);
     }
+
+    @PutMapping("/update")
+    public ResponseEntity updateUser(@AuthenticationPrincipal UserEntity user,
+                                     @RequestBody UserRequestDto request) {
+        userService.updateUser(user, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity deleteUser(@AuthenticationPrincipal UserEntity user) {
+        userService.deleteUser(user);
+        return ResponseEntity.ok().build();
+    }
+
 }
