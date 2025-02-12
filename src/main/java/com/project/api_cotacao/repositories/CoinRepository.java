484ff394f53b0d9dd9737edb1fda3a9f4ce0b854
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-02-12 12:13:31 -0300

    feat: Updating coin repository

--- src/main/java/com/project/api_cotacao/repositories/CoinRepository.java
+++ src/main/java/com/project/api_cotacao/repositories/CoinRepository.java
@@ -1,7 +1,24 @@
 package com.project.api_cotacao.repositories;
 
 import com.project.api_cotacao.entities.coin.CoinEntity;
+import com.project.api_cotacao.entities.wallet.WalletEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CoinRepository extends JpaRepository<CoinEntity, Long> {
+
+    List<CoinEntity> findAllByWallet(WalletEntity wallet);
+
+    @Query("SELECT c FROM CoinEntity c WHERE c.wallet.user.id = :userId")
+    List<CoinEntity> findAllByUserId(@Param("userId") Long userId);
+
+    CoinEntity findByWalletAndCode(WalletEntity wallet, String code);
+
+    @Query("SELECT c FROM CoinEntity c WHERE c.wallet = :wallet AND c.isPrincipal = true")
+    Optional<CoinEntity> findPrincipalCoinByWallet(@Param("wallet") WalletEntity wallet);
+
 }
