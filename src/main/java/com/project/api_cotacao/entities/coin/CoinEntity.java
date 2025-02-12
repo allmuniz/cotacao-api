ef2203de6ac56e2e4b80466cd911ba84de864a47
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-02-12 16:20:45 -0300

    feat: Updating all entities

--- src/main/java/com/project/api_cotacao/entities/coin/CoinEntity.java
+++ src/main/java/com/project/api_cotacao/entities/coin/CoinEntity.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class CoinEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String code;
